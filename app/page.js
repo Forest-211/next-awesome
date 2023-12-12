38a9e8f809ca6007795347e39ccac5b533633af7
Forest <767425412lin@gmail.com>
2023-12-12 09:52:55 +0800

    feat: app router

--- app/page.js
+++ app/page.js
@@ -3,8 +3,6 @@ import {Profiler} from "react";
 
 export default function Home() {
   return (
-    <Profiler id="hello">
-      <p>hello app server</p>
-    </Profiler>
+    <h1>Hello, Next.js!</h1>
   )
 }
