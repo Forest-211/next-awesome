4bfa54575f0e1cf29f228a168bb08e1a71b07059
Forest <767425412lin@gmail.com>
2024-12-16 18:31:12 +0800

    feat: env example

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
 import Image from "next/image";
 
 export default function Home() {
+  console.log('process.env.DB_HOST', process.env.DB_HOST);
+  console.log('process.env.DB_USER', process.env.DB_USER);
+  console.log('process.env.DB_PASS', process.env.DB_PASS);
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
