45982ee7349c53ab206e70cbfa0baf79abb60698
Forest <767425412lin@gmail.com>
2024-12-17 18:01:17 +0800

    feat: 添加环境变量相关演示代码

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Client from './components/client';
 
 export default function Home() {
   console.log('process.env.DB_HOST', process.env.DB_HOST);
@@ -25,7 +26,8 @@ export default function Home() {
           </li>
           <li>Save and see your changes instantly.</li>
         </ol>
-
+        <p>process env NODE_ENV: {process.env.NODE_ENV}</p>
+        <Client />
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
