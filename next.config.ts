7a7031a631b642b23537250d736d00792ff02655
Forest <767425412lin@gmail.com>
2024-12-25 11:51:22 +0800

    feat: next.config.ts 相关配置

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     images: {
+        minimumCacheTTL: 60,
+        dangerouslyAllowSVG: true,
+        contentDispositionType: 'attachment',
+        contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+        // disableStaticImages: true,
+        formats: ['image/avif', 'image/webp'],
         localPatterns: [
             {
                 pathname: '/assets/images/**',
@@ -26,6 +32,8 @@ const nextConfig: NextConfig = {
         ],
         // loader: 'custom',
         // loaderFile: './my/image/loader.js',
+        deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+        imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     },
 };
 
--- src/app/image/page.tsx
+++ src/app/image/page.tsx
@@ -7,6 +7,7 @@ export default function page() {
             <Image
                 src={img}
                 alt="Picture of the author"
+                sizes="(max-width: 600px) 160px, 320px"
             // width={500} automatically provided
             // height={500} automatically provided
             // blurDataURL="data:..." automatically provided
